56e29327558acafd674db0bb24ce8c4264627515
Fashakin Olashubomi <subomifasakin@outlook.com>
2026-02-20 02:34:34 +0100

    edited auth

--- app/auth/sign-in/page.tsx
+++ app/auth/sign-in/page.tsx
@@ -18,7 +18,7 @@ export default function SignInPage() {
             </h1>
 
             <div className="space-y-2">
-              <p className="text-sm font-light text-muted-foreground">
+              <p className="text-sm font-light tracking-tight text-muted-foreground">
                 Let&apos;s get you signed in to start sharing files securely.
               </p>
             </div>
@@ -40,10 +40,12 @@ export default function SignInPage() {
               <path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l2.85-2.22.81-.62z" />
               <path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" />
             </svg>
-            <span className="font-semibold">Continue with Google</span>
+            <span className="font-semibold tracking-tight">
+              Continue with Google
+            </span>
           </Button>
 
-          <p className="text-xs font-light text-center text-muted-foreground">
+          <p className="text-xs font-light tracking-tight text-center text-muted-foreground">
             By signing in, you agree to our{" "}
             <Link
               href="/terms-of-service"
